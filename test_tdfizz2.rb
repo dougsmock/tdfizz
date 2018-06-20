a869004ff455365d42307389d29a81ef98515b8d
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-20 16:42:47 -0400

    Add files via upload

--- test_tdfizz2.rb
+++ test_tdfizz2.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require_relative "Calculator2.rb"
+require_relative "calculator2.rb"
 
 class TestTddfizzbuzz < Minitest::Test
 
@@ -7,5 +7,17 @@ class TestTddfizzbuzz < Minitest::Test
       assert_equal(1, 1)
     end
 
-    def test_assert_if_plus_func_called
-      assert_math()
+    def test_assert_if_5_plus_20_equals_25
+      assert_equal(25, addition (5, 20))
+    end
+
+    def test_assert_if_12_plus_18_equals_40
+      assert_equal(40, addition (12, 28))
+    end
+
+    def test_assert_if_12_7_plus_23_4_equals_36_1
+      assert_equal(36.1, addition(23.4, 36.1))
+
+
+    end
+end
