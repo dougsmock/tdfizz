c1022e5133925d9342a6b1589b2be9d79714d1da
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-20 15:24:28 -0400

    Add files via upload

--- test_td_fizzbuzz.rb
+++ test_td_fizzbuzz.rb
@@ -1,13 +1,24 @@
 require "minitest/autorun"
 require_relative "tdfizz.rb"
 
-class TestTdfizz < Minitest::Test
+class TestYourFunctions < Minitest::Test
 
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
 
-    def test_result_is_Array()
+    def test_result_is_array
       assert_equal(Array, fizzbuzz().class)
+    end
 
+    def test_array_length_is_100
+      assert_equal(100, fizzbuzz().length)
+    end
+
+    def test_array_first_element_returns_1
+      assert_equal(1, fizzbuzz()[0])
+
+      def test_array_third_element_mined
+        assert_equal("mined", fizzbuzz()[2])
+    end
 end
