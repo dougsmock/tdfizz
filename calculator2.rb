0af467879df1db7261bcb2aad47c318a22f37329
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-24 00:39:26 -0400

    Add files via upload

--- calculator2.rb
+++ calculator2.rb
@@ -1,27 +1,31 @@
 def plus(a, b)
-  puts "Answer is #{a} + #{b}"
-  return a + b
-
-
-
-
-
+  print "Let's add two numbers. Give me one: "
+  a = gets.chomp.to_i
+  check_string(a)
+  print "Give me the other: "
+  b = gets.chomp.to_i
+  check_string(b)
+  answer = a + b
+  print "#{answer}\n\n"
+  repeat()
 end
 
 
-#starting point
-puts "The following is a 4-function calculator."
-puts "Put (a) add, (s) subtract, (m) multiply, (d) divide."
-choice = gets.chomp.to_s
-    if choice == 'add'
-      plus(5.0, 20.0)
-  #  elsif choice.downcase == 'subtract'
-  #    minus(choice)
-  #  elsif choice.downcase == 'multiply'
-  #    times(choice)
-  #  elsif choice.downcase == 'divide'
-  #    divideby(choice)
-  #  else
-  #    puts "You have a typo. Try again."
-  #    repeat()
-    end
+
+
+
+
+# puts "You want to add, subtract, multiply or divide? "
+#   choice = gets.chomp
+#     if choice.downcase == 'add'
+#       plus(choice)
+#     elsif choice.downcase == 'subtract'
+#       minus(choice)
+#     elsif choice.downcase == 'multiply'
+#       times(choice)
+#     elsif choice.downcase == 'divide'
+#       divideby(choice)
+#     else
+#       puts "You have a typo. Try again."
+#       repeat()
+#     end
